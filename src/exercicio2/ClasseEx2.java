37cecfc633a915514f47dfe40e6155baa1672202
Tales Godinho <talesflg@hotmail.com>
2021-12-06 11:41:48 -0300

    Exercício 2

---
+++ src/exercicio2/ClasseEx2.java
@@ -0,0 +1,13 @@
+package exercicio2;
+
+public class ClasseEx2 {
+    private String nome;
+
+    public ClasseEx2(String nome){
+        this.nome = nome;
+    }
+
+    public ClasseEx2(){
+        this("Teste");
+    }
+}
